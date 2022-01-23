25bd7a1a44b45fb23c5526744302eb03675459c0
sukchuli <98261294+sukchuli@users.noreply.github.com>
2022-01-23 21:05:13 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -11,6 +11,7 @@ function doLogin(event) {
   const loginName = loginform.querySelector("input");
   localStorage.setItem(KEY_LOGIN_NAME, loginName.value);
   logout.querySelector("span:first-child").innerText = `안녕하세요! ${loginName.value}`;
+  
   loginName.value = "";
   login.classList.add(KEY_CLASS_HIDDEN);
   logout.classList.remove(KEY_CLASS_HIDDEN);
