ff97a00b3e2249280223e1bdaaa03cd1d081243b
sukchuli <98261294+sukchuli@users.noreply.github.com>
2022-01-23 21:02:22 +0900

    Add files via upload

--- js/geolocation.js
+++ js/geolocation.js
@@ -7,7 +7,6 @@ function onGeoOk(position) {
   const lon = position.coords.longitude;
   
   const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lang=kr&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  console.log(url);
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
